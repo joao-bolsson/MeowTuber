8fa52ea28fee6a89f90f604367af406eecea8eaf
João Bolsson <joaovictorbolsson@gmail.com>
2019-12-03 00:12:33 -0300

    script para instalar dependencias

--- NFQUEUE/create_interface.sh
+++ NFQUEUE/create_interface.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-ifconfig enp0s3:0 10.0.2.16
-ifconfig enp0s3:0 netmask 255.255.255.0
+## codigo original
 
-ifconfig enp0s3:1 10.0.2.17
-ifconfig enp0s3:1 netmask 255.255.255.0
+#ifconfig enp0s3:0 10.0.2.16
+#ifconfig enp0s3:0 netmask 255.255.255.0
+
+#ifconfig enp0s3:1 10.0.2.17
+#ifconfig enp0s3:1 netmask 255.255.255.0
+
+## fim do codigo original
+
+# se o teu ip eh 192.168.15.5 a configuracao eh a abaixo:
+ifconfig wlp2s0:0 192.168.15.6
+ifconfig wlp2s0:0 netmask 255.255.255.0
+
+ifconfig wlp2s0:1 192.168.15.7
+ifconfig wlp2s0:1 netmask 255.255.255.0
---
+++ install_deps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# instalando as dependencias
+
+sudo apt install curl
+sudo apt install python3-pip
+sudo apt install scapy
+sudo npm install -g http-perf
+
+# instala as dependencias para netfilter
+sudo apt-get install build-essential python-dev libnetfilter-queue-dev
+
+# instala o netfilter
+sudo pip install NetfilterQueue
+
+# OK tudo instalado
+
